1cb27d63f48a1dea381cbde0fa8da7b04c61258d
abdelmounim <124405682+abdelmounim-moumay@users.noreply.github.com>
2025-01-01 20:13:06 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,1 +1,19 @@
+def buildApp() {
+    echo 'building app'
+}
+
+def testApp() {
+    echo 'testin app'
+}
+
+def deployApp() {
+    echo 'deploying app'
+    echo "deploying version ${params.VERSION}"
+}
+
+return this
+
+
+
+
 
